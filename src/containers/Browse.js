57b627feb95f5b227007e06857e74d1f6b646f97
Brian <br.y.an@hotmail.com>
2021-04-23 15:46:48 -0300

    creando perfiles utilizando context de firebase

--- src/containers/Browse.js
+++ src/containers/Browse.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { SelectProfileContainer } from './Profiles'
+import { FirebaseContext } from '../context/firebase'
 
-export function BrowseContainer() {
+export function BrowseContainer({ slides }) {
+    const { firebase } = useContext(FirebaseContext)
+    const user = firebase.auth().currentUser || {};
+    
     return (
-        <p>Hi</p>
+        <SelectProfileContainer />
     )
 }
