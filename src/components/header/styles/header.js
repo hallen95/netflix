4a1655b76a5380ffc8fa90126cbac62f4a1b1276
Brian <br.y.an@hotmail.com>
2021-04-29 18:32:32 -0300

    creating browser header links

--- src/components/header/styles/header.js
+++ src/components/header/styles/header.js
@@ -8,6 +8,10 @@ export const Background = styled.div`
         (src ? `../images/misc/${src}.jpg` 
         : '../images/misc/home-bg.jpg')}) 
         top left /cover no-repeat;
+
+    @media (max-width: 1100px) {
+        ${({ dontShowOnSmallViewPort }) => dontShowOnSmallViewPort && `background: none;`}
+    }
 `;
 
 export const Container = styled.div`
@@ -73,4 +77,30 @@ export const Text = styled.p`
     line-height: normal;
     text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.45);
     margin: 0;
+`;
+
+export const Link = styled.p`
+    color: white;
+    text-decoration: none;
+    margin-right: 30px;
+    font-weight: ${({ active }) =>(active === 'true' ? '700': 'normal')};
+    cursor: pointer;
+
+    &:hover {
+        font-weight: bold;
+    }
+
+    &:last-of-type {
+        margin-right: 0;
+    }
+`;
+
+export const FeatureCallOut = styled.h2`
+    color: white;
+    font-size: 50px;
+    line-height: normal;
+    font-weight: bold;
+    text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.45);
+    margin: 0;
+    margin-bottom: 20px;
 `;
